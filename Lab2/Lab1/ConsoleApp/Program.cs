8fd365a81e3b08b8abc9cfaadaef7b957a9c34d4
i3artek <bartekwzorek@onet.pl>
2023-03-09 15:57:11 +0100

    one

---
+++ Lab2/Lab1/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Library;
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const string xml_file = "../../../../Library/example_library.xml";
+            try
+            {
+                LibraryReader.ValidateFile(xml_file);
+            }
+            catch
+            {
+                return;
+            }
+            var myLib = LibraryReader.ReadLibrary(xml_file);
+            foreach(var b in myLib.book)
+            {
+                foreach(var a in b.authors)
+                {
+                    Console.WriteLine(a.surname);
+                }
+            }
+        }
+    }
+}
---
+++ Lab2/Lab1/Library/LibraryReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Library
+{
+    public static class LibraryReader
+    {
+        public static libraryType ReadLibrary(string PathToFile)
+        {
+            var xs = new XmlSerializer(typeof(libraryType));
+            libraryType ret;
+            using (var reader = new FileStream(PathToFile, FileMode.Open))
+            {
+                ret = (libraryType)xs.Deserialize(reader);
+            }
+           
+            return ret;
+        }
+
+        public static void ValidateFile(string PathToFile)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            // Validator settings
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            // Here we add xsd files to namespaces we want to validate
+            // (It's like XML -> Schemas setting in Visual Studio)
+            settings.Schemas.Add("library", "../../../../Library/library.xsd");
+
+            // Processing XSI Schema Location attribute
+            // (Disabled by default as it is a security risk). 
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+
+            // A function delegate that will be called when 
+            // validation error or warning occurs
+            settings.ValidationEventHandler += ValidationHandler;
+
+            using (XmlReader reader = XmlReader.Create(PathToFile, settings))
+            {
+                // Read method reads next element or attribute from the document
+                // It will call ValidationEventHandler if some invalid
+                // part occurs
+                while (reader.Read())
+                {
+                }
+            }
+        }
+
+        private static void ValidationHandler(Object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("Warning: {0}", args.Message);
+            else
+                Console.WriteLine("Error: {0}", args.Message);
+
+
+            throw new Exception();
+        }
+
+    }
+}
