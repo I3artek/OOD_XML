80f62a853ea5c6202e7795f4ee65cc37559bd58c
i3artek <bartekwzorek@onet.pl>
2023-03-09 19:32:53 +0100

    xd

---
+++ Garden/GardenConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using GardenLibrary;
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var xml_file = Environment.GetCommandLineArgs()[1];
+            try
+            {
+                GardenReader.ValidateFile(xml_file);
+            }
+            catch
+            {
+                return;
+            }
+            var myLib = GardenReader.ReadFromFile(xml_file);
+            foreach(var b in myLib.book)
+            {
+                foreach(var a in b.authors)
+                {
+                    Console.WriteLine(a.surname);
+                }
+            }
+        }
+    }
+}
---
+++ Garden/GardenLibrary/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace GardenLibrary;
+
+public static class GardenReader
+{
+    public static libraryType ReadFromFile(string PathToFile)
+    {
+        var xs = new XmlSerializer(typeof(libraryType));
+        libraryType ret;
+        using (var reader = new FileStream(PathToFile, FileMode.Open))
+        {
+            ret = (libraryType)xs.Deserialize(reader);
+        }
+           
+        return ret;
+    }
+    
+    public static void ValidateFile(string PathToFile)
+    {
+        XmlReaderSettings settings = new XmlReaderSettings();
+        // Validator settings
+        settings.ValidationType = ValidationType.Schema;
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+        // Here we add xsd files to namespaces we want to validate
+        // (It's like XML -> Schemas setting in Visual Studio)
+        settings.Schemas.Add("library", "../../../../Library/library.xsd");
+
+        // Processing XSI Schema Location attribute
+        // (Disabled by default as it is a security risk). 
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+
+        // A function delegate that will be called when 
+        // validation error or warning occurs
+        settings.ValidationEventHandler += ValidationHandler;
+
+        using (XmlReader reader = XmlReader.Create(PathToFile, settings))
+        {
+            // Read method reads next element or attribute from the document
+            // It will call ValidationEventHandler if some invalid
+            // part occurs
+            while (reader.Read())
+            {
+            }
+        }
+    }
+
+    private static void ValidationHandler(Object sender, ValidationEventArgs args)
+    {
+        if (args.Severity == XmlSeverityType.Warning)
+            Console.WriteLine("Warning: {0}", args.Message);
+        else
+            Console.WriteLine("Error: {0}", args.Message);
+
+
+        throw new Exception();
+    }
+}
